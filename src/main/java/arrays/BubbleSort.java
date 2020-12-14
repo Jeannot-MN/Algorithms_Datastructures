38e93d38c467b1e13a6f9743755dcab70fb7a8f6
JeannotMN <jeannot@ssquared.dev>
2020-12-14 12:10:48 +0200

    Add some description for each algorithm

--- src/main/java/arrays/BubbleSort.java
+++ src/main/java/arrays/BubbleSort.java
@@ -2,6 +2,12 @@ package arrays;
 
 import static arrays.ArrayApplication.swap;
 
+/*
+    Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent
+    elements if they are in wrong order. The algorithm maintains two subarrays in a given array.
+
+    upi: unsorted partition index
+*/
 public class BubbleSort {
     public static void main(String[] args) {
 
@@ -9,8 +15,9 @@ public class BubbleSort {
 
         for (int upi = array.length - 1; upi > 0; upi--) {
             for (int i = 0; i < upi; i++) {
-                if (array[i] > array[i + 1])
+                if (array[i] > array[i + 1]){
                     swap(i, i + 1, array);
+                }
             }
         }
 
--- src/main/java/arrays/SelectionSort.java
+++ src/main/java/arrays/SelectionSort.java
@@ -2,6 +2,13 @@ package arrays;
 
 import static arrays.ArrayApplication.swap;
 
+/*
+    The selection sort algorithm sorts an array by repeatedly finding the largest element (considering ascending order)
+    from unsorted part and putting it at the end of the unsorted array partition.
+     The algorithm maintains two subarrays in a given array.
+
+    upi: unsorted partition index
+ */
 public class SelectionSort {
 
     public static void main(String[] args) {
@@ -10,8 +17,9 @@ public class SelectionSort {
         for (int upi = array.length - 1; upi > 0; upi--) {
             int largest_index = 0;
             for (int i = 0; i <= upi; i++) {
-                if (array[i] > array[largest_index])
+                if (array[i] > array[largest_index]){
                     largest_index = i;
+                }
             }
             swap(largest_index, upi, array);
         }
